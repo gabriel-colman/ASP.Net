a5eea745426fbefe65c1c463566aef9767bbf39a
Gabril Colman <gabriecolman7@gmail.com>
2025-04-07 16:46:12 -0400

    Exemplo

--- ExemploASP_NET_CORE/Program.cs
+++ ExemploASP_NET_CORE/Program.cs
@@ -1,44 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+using ExemploASP_NET_CORE.database;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder(args); // Criar a aplicação web
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Servicos de configuração
+builder.Services.AddControllersWithViews(); // Adiciona o MVC com Views
+builder.Services.AddEndpointsApiExplorer(); // Adiciona o explorador de endpoints
+builder.Services.AddSwaggerGen(); // Adiciona o Swagger para documentação da API
+builder.Services.AddDbContext<AppDbContext>(options => // Configura o contexto do banco de dados
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")); // Usa o PostgreSQL com a string de conexão definida no appsettings.json
+});
+
+var app = builder.Build(); // Cria a aplicação a partir dos serviços configurado
+// 
+
+if (app.Environment.IsDevelopment()) // Verifica se o ambiente é de desenvolvimento
+{
+    app.UseSwagger(); // Habilita o Swagger
+    app.UseSwaggerUI(); // Habilita a interface do Swagger
 }
 
-app.UseHttpsRedirection();
+app.UseDefaultFiles(); // Habilita os arquivos padrão (index.html, index.htm, default.html)
+app.UseStaticFiles(); // Habilita os arquivos estáticos (CSS, JS, imagens, etc.)
+app.UseHttpsRedirection(); // Redireciona requisições HTTP para HTTPS
+app.MapControllers(); // Mapeia os controladores para as rotas
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run(); // Executa a aplicação
