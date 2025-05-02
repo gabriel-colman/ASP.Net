5714f7d4fa563172401179ecd75b12c95980c85f
Gabril Colman <gabriecolman7@gmail.com>
2025-05-02 15:36:16 -0400

    Swagger

--- SistemaEscolarAPI/Program.cs
+++ SistemaEscolarAPI/Program.cs
@@ -14,28 +14,27 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Comando para criar os migrations no banco de dados
-// dotnet ef migrations add InitialCreate
-// dotnet ef database update
-
 // Configurar o DbContext com PostgreSQL (usando appsettings.json)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
-var app = builder.Build();
-
+// Adicionar serviços ao contêiner
+builder.Services.AddControllers(); // Adiciona suporte a controladores
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
+builder.Services.AddSwaggerGen(c => // para gerar a documentação lá em baixar, que é o Schemas
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sistema Escolar API", Version = "v1" }); // Define o título e a versão da API na documentação do Swagger
 });
 
+var app = builder.Build();
+
+// Configurar middlewares
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
 // app.UseAuthentication();
 // app.UseAuthorization();
-app.MapControllers();
+app.MapControllers(); // Mapeia os controladores
 app.Run();
+
+// Link para Swagger: http://localhost:5164/swagger/index.html
